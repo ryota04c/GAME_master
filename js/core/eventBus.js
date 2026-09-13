fc40c28f67fe913a868b5aae35c2a7c9ed24bc90
ryota04c <135027002+ryota04c@users.noreply.github.com>
2026-09-13 20:57:45 +0900

    Update eventBus.js

--- js/core/eventBus.js
+++ js/core/eventBus.js
@@ -1,1 +1,25 @@
+const events = {};
 
+
+// イベントを登録する
+export function on(eventName, callback) {
+
+    if (!events[eventName]) {
+        events[eventName] = [];
+    }
+
+    events[eventName].push(callback);
+}
+
+
+// イベントを発生させる
+export function emit(eventName, data = null) {
+
+    if (!events[eventName]) {
+        return;
+    }
+
+    events[eventName].forEach(callback => {
+        callback(data);
+    });
+}
