35c513f9d0cbe10c6f4f0cd7565506f154d6afe4
ryota04c <135027002+ryota04c@users.noreply.github.com>
2026-09-13 20:52:24 +0900

    Update gameState.js

--- js/core/gameState.js
+++ js/core/gameState.js
@@ -1,1 +1,17 @@
+export const gameState = {
+    // 現在のゲーム状態
+    state: "setting",
 
+    // ゲーム情報
+    game: {
+        name: "",
+        round: 0,
+        maxRounds: Infinity
+    },
+
+    // プレイヤー
+    players: [],
+
+    // 現在のプレイヤー
+    currentPlayer: null
+};
